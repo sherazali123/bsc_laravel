a1772d5ae330c332a434d455fdedf6cdefac3350
khalidshah <khalidshah.webmaster@gmail.com>
2015-08-17 20:05:02 +0500

    change layout

--- resources/views/dimensions/_form.blade.php
+++ resources/views/dimensions/_form.blade.php
@@ -1,17 +1,18 @@
 <p>
-    {!! Form::label('Name', 'Name:') !!}
-    <span class="field">
-        {!! Form::text('name',null,['class'=>'input-xxlarge', 'placeholder' => 'Name']) !!}
-    </span>
-</p>
-<p>
     {!! Form::label('Strategic plan', 'Strategic plan:') !!}
     <span class="field">
          {!! Form::select('plan_id', $plans, null, ['class' => 'form-control']) !!}
     </span>
 </p>
 <p>
-    {!! Form::label('description', 'Name:') !!}
+    {!! Form::label('Enter dimension name', 'Name:') !!}
+    <span class="field">
+        {!! Form::text('name',null,['class'=>'input-xxlarge', 'placeholder' => 'Name']) !!}
+    </span>
+</p>
+
+<p>
+    {!! Form::label('Enter dimension description', 'Name:') !!}
     <span class="field">
         {!! Form::textarea('description',null,['class'=>'span6', 'placeholder' => 'Description']) !!}
     </span>
