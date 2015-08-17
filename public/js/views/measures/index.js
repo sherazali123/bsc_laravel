8f6f7c3bf19e1dfc3126f715d6dfd76f7f530ea2
khalidshah <khalidshah.webmaster@gmail.com>
2015-08-17 20:30:00 +0500

    search

--- public/js/views/measures/index.js
+++ public/js/views/measures/index.js
@@ -1,17 +1,37 @@
 jQuery.noConflict();
 
-jQuery(document).ready(function(){
+jQuery(document).ready(function() {
 
 
-	// dynamic table
-	if(jQuery('#index_1').length > 0) {
-		jQuery('#index_1').dataTable({
-			"sPaginationType": "full_numbers",
-			"aaSortingFixed": [[0,'asc']],
-			// "fnDrawCallback": function(oSettings) {
-			// 	jQuery.uniform.update();
-			// }
-		});
-	}
+    // dynamic table
+    if (jQuery('#index_1').length > 0) {
+        jQuery('#index_1').dataTable({
+            "sPaginationType": "full_numbers",
+            "aaSortingFixed": [[0, 'asc']],
+            // "fnDrawCallback": function(oSettings) {
+            // 	jQuery.uniform.update();
+            // }
+            initComplete: function() {
+                this.api().columns().every(function() {
+                    var column = this;
+                    var select = $('<select><option value=""></option></select>')
+                            .appendTo($(column.footer()).empty())
+                            .on('change', function() {
+                                var val = $.fn.dataTable.util.escapeRegex(
+                                        $(this).val()
+                                        );
+
+                                column
+                                        .search(val ? '^' + val + '$' : '', true, false)
+                                        .draw();
+                            });
+
+                    column.data().unique().sort().each(function(d, j) {
+                        select.append('<option value="' + d + '">' + d + '</option>')
+                    });
+                });
+            }
+        });
+    }
 
 });
