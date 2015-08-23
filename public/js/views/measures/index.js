395416d7ba74bf5f07e069e89db206844a8a04a6
khalidshah <khalidshah.webmaster@gmail.com>
2015-08-23 23:23:14 +0500

    zsczC

--- public/js/views/measures/index.js
+++ public/js/views/measures/index.js
@@ -1,18 +1,17 @@
 jQuery.noConflict();
 
-jQuery(document).ready(function() {
+jQuery(document).ready(function(){
 
 
-    // dynamic table
-    if (jQuery('#index_1').length > 0) {
-        jQuery('#index_1').dataTable({
-            "sPaginationType": "full_numbers",
-            "aaSortingFixed": [[0, 'asc']],
-            // "fnDrawCallback": function(oSettings) {
-            // 	jQuery.uniform.update();
-            // }
-  
-        });
-    }
+	// dynamic table
+	if(jQuery('#index_1').length > 0) {
+		jQuery('#index_1').dataTable({
+			"sPaginationType": "full_numbers",
+			"aaSortingFixed": [[0,'asc']],
+			// "fnDrawCallback": function(oSettings) {
+			// 	jQuery.uniform.update();
+			// }
+		});
+	}
 
 });
