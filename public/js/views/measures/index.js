00e5ef9b2052edd72f459e9efb905223af23a560
khalidshah <khalidshah.webmaster@gmail.com>
2015-08-23 23:21:08 +0500

    zxcz

--- public/js/views/measures/index.js
+++ public/js/views/measures/index.js
@@ -11,24 +11,7 @@ jQuery(document).ready(function() {
             // "fnDrawCallback": function(oSettings) {
             // 	jQuery.uniform.update();
             // }
-            "initComplete": function() {
-                this.api().columns().every(function() {
-                    var column = this;
-                    var select =jQuery('<select><option value=""></option></select>')
-                            .appendTo(jQuery(column.footer()).empty())
-                            .on('change', function() {
-                                var val = jQuery.fn.dataTable.util.escapeRegex(
-                                        jQuery(this).val()
-                                        );
-
-                                column.search(val ? '^' + val + '$' : '', true, false).draw();
-                            });
-
-                    column.data().unique().sort().each(function(d, j) {
-                        select.append('<option value="' + d + '">' + d + '</option>')
-                    });
-                });
-            }
+  
         });
     }
 
