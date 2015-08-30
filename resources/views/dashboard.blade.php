1d3291d296b0d2d7177c36e7e44c8801ef8c5d66
Sheraz Ali <sheraz.ali342@gmail.com>
2015-08-30 16:51:42 +0500

    Removed extra header from the dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,20 +7,7 @@
 @endsection
 
 @section('main')
- 	    <div class="breadcrumbwidget">
-            <ul class="skins">
-                <li><a href="default" class="skin-color default"></a></li>
-                <li><a href="orange" class="skin-color orange"></a></li>
-                <li><a href="dark" class="skin-color dark"></a></li>
-                <li>&nbsp;</li>
-                <li class="fixed selected"><a href="#" class="skin-layout fixed"></a></li>
-                <li class="wide"><a href="#" class="skin-layout wide"></a></li>
-            </ul><!--skins-->
-           <!--  <ul class="breadcrumb">
-                <li><a href="dashboard.html">Home</a> <span class="divider">/</span></li>
-                <li class="active">Dashboard</li>
-            </ul> -->
-        </div><!--breadcrumbs-->
+ 
       
 	<div class="pagetitle">
             <h1>Dashboard</h1> <span>Dashboard page display all plans</span>
