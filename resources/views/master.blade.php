be917c0059d79a0b1b6c1444f345f9ec79c1a935
khalidshah <khalidshah.webmaster@gmail.com>
2015-08-31 15:48:12 +0500

    bbn

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -100,7 +100,7 @@
     <div class="rightpanel">
         <div class="headerpanel" style="min-height: 89px;">
             <a href="#" class="showmenu" style="float: left;"></a>
-<div style="font-size: 18px; color: #FFF;font-family: 'RobotoBold', 'HelveticaNeue', Arial, sans-serif;width: 67%;display: block;float: left;margin-left: 60px;margin-top: 25px;text-align: center;position: relative;">Implement your Strategic Plan using Balance Score Card An enterprise performance management solution by Dr.Rashed Al-jalahma</div>
+            <div style="font-size: 18px; color: #FFF;font-family: 'RobotoBold', 'HelveticaNeue', Arial, sans-serif;width: 67%;display: block;float: left;margin-left: 60px;margin-top: 25px;text-align: center;position: relative;">Implement your Strategic Plan using Balance Score Card <br/>An enterprise performance management solution by Dr.Rashed Al-jalahma</div>
             <div class="headerright">
                 <div class="dropdown notification">
                  
