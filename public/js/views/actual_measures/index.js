c2255f0215f6996a7425c5512ea533e2c19075c7
khalidshah <khalidshah.webmaster@gmail.com>
2015-09-03 17:12:15 +0500

    add graph

--- public/js/views/actual_measures/index.js
+++ public/js/views/actual_measures/index.js
@@ -49,7 +49,7 @@ jQuery(document).ready(function() {
                 type: 'logarithmic',
                 minorTickInterval: 0,
                 max: 100,
-                min: 0
+                min: 1
             }, {// Secondary yAxis
                 title: {
                     text: graphData.columnName,
