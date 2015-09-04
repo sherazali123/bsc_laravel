15fb090a34c9cfacbef7e9f6e467dd88ef38494e
khalidshah <khalidshah.webmaster@gmail.com>
2015-09-04 11:08:10 +0500

    add graph

--- public/js/views/dimensions/show.js
+++ public/js/views/dimensions/show.js
@@ -60,8 +60,9 @@ jQuery(document).ready(function() {
                     }
                 },
                 type: 'logarithmic',
-                 minorTickInterval: 0.1,
+                 minorTickInterval: 0,
                 max: 100,
+                min:0.1,
             },{// Secondary yAxis
                 title: {
                     text: graphData.splineName,
